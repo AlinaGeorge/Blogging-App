4c113db0c26278a51b0bb1ceddcd833ab68627d8
Alinageorge <alinagrg03@gmail.com>
2025-06-06 17:31:22 +0530

    Setting up token authentication

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework.serializers import ModelSerializer
 
-# Create your views here.
+# User Serializer
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+# Register View
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]  
